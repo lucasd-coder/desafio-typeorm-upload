fbe04577fa5e715eb3c009ec8b9105bac75d9108
Lucas da silva <lucasdasilva524@hotmail.com>
2020-11-29 19:30:49 -0300

    Desafio resolvido

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,32 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transaction = await this.find();
+
+    const { income, outcome } = transaction.reduce(
+      (accamulator: Balance, transactions: Transaction) => {
+        switch (transactions.type) {
+          case 'income':
+            accamulator.income += Number(transactions.value);
+            break;
+          case 'outcome':
+            accamulator.outcome += Number(transactions.value);
+            break;
+          default:
+            break;
+        }
+        return accamulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
